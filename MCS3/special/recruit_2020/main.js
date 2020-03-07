80a61e6655ad5d968ac3fa264f90dc4b12045bd9
takeda123 <58212908+takeda123@users.noreply.github.com>
2020-03-07 22:46:46 +0900

    change
    triangle修正

--- MCS3/special/recruit_2020/main.js
+++ MCS3/special/recruit_2020/main.js
@@ -42,36 +42,45 @@ $(function(){
     });
     });
   // ボタンスクロール
-  $('a[href^="#"]').click(function(){
-    var speed = 600;
-    var href= $(this).attr("href");
-    var target = $(href == "#how-to" || href == "id=how-to" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-  });
+  // $('a[href^="#"]').click(function(){
+  //   var speed = 600;
+  //   var href= $(this).attr("href");
+  //   var target = $(href == "#how-to" || href == "id=how-to" ? 'html' : href);
+  //   var position = target.offset().top;
+  //   $("html, body").animate({scrollTop:position}, speed, "swing");
+  //   return false;
+  // });
 
-  $('a[href^="#"]').click(function(){
-    var speed = 600;
-    var href= $(this).attr("href");
-    var target = $(href == "#detail" || href == "id=detail" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-  });
-
-
-  $('a[href^="#"]').click(function(){
-    var speed = 600;
-    var href= $(this).attr("href");
-    var target = $(href == "#qa" || href == "id=qa" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-  });
+  // $('a[href^="#"]').click(function(){
+  //   var speed = 600;
+  //   var href= $(this).attr("href");
+  //   var target = $(href == "#detail" || href == "id=detail" ? 'html' : href);
+  //   var position = target.offset().top;
+  //   $("html, body").animate({scrollTop:position}, speed, "swing");
+  //   return false;
+  // });
 
 
+  // $('a[href^="#"]').click(function(){
+  //   var speed = 600;
+  //   var href= $(this).attr("href");
+  //   var target = $(href == "#qa" || href == "id=qa" ? 'html' : href);
+  //   var position = target.offset().top;
+  //   $("html, body").animate({scrollTop:position}, speed, "swing");
+  //   return false;
+  // });
 
 
-  });
+
+
+  $(function () {
+    $('a[href^=#]').click(function() {
+         var speed = 400;
+         var href= $(this).attr("href");
+         var target = $(href == "#" || href == "" ? 'html' : href);
+         var position = target.offset().top;
+         $('body,html').animate({scrollTop:position}, speed, 'swing');
+         return false;
+    });
+ });
 
