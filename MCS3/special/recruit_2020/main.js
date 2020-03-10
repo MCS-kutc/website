a6f4824320d841286f85b000116c750b7c93bf6d
masaki02 <58213014+masaki02@users.noreply.github.com>
2020-03-10 14:25:53 +0900

    Revert "Merge branch 'master' of https://github.com/MCS-kutc/website"
    This reverts commit 4770ba2a6fd0c9a3db0b88e8229eb1ba258764ba, reversing
    changes made to 4fcdabe3ac1c3740f776b8f3e232aeaf2bfb8e84.

--- MCS3/special/recruit_2020/main.js
+++ MCS3/special/recruit_2020/main.js
@@ -1,85 +1,79 @@
-// $(function () {
-  $(function () {
 
-        // メニューを閉じる
-         // クリック時の動作
-      $('.hamberger_line').on('click', function() {
-        // メニューを閉じる
-        if($(this).hasClass('open')) {
-            $(this).removeClass('open');
-            $('.hamberger_list').removeClass('open');
-        // メニューを開く
-        } else {
-            $(this).addClass('open');
-            $('.hamberger_list').addClass('open');
-        }
+$(function () {
+$(function () {
+
+      // メニューを閉じる
+       // クリック時の動作
+    $('.menu-btn').on('click', function() {
+      // メニューを閉じる
+      if($(`.menu`).hasClass('is-active')){
+          $(`.menu`).removeClass('is-active');
+          $('.menu').removeClass('is-active');
+      // メニューを開く
+      } else {
+          $(`.menu`).addClass('is-active');
+          $('.menu').addClass('is-active');
+      }
 
   });
-    });
+  });
+
+
+//■page topボタン
 $(function(){
-  var hamberger_menu=$('.hamberger_menu');
-  $(window).scroll(function(){
-    if($(this).scrollTop()>40){
-      hamberger_menu.hide();
-    }else{
-      hamberger_menu.fadeIn();
-    }
-  })
-});
-
-  //■page topボタン
-  $(function(){
-        var topBtn=$('#pageTop');
-        topBtn.hide();
+      var topBtn=$('#pageTop');
+      topBtn.hide();
 
 
 
-        //◇ボタンの表示設定
-        $(window).scroll(function(){
-          if($(this).scrollTop()>80){
+      //◇ボタンの表示設定
+      $(window).scroll(function(){
+        if($(this).scrollTop()>80){
 
-            //---- 画面を80pxスクロールしたら、ボタンを表示する
-            topBtn.fadeIn();
+          //---- 画面を80pxスクロールしたら、ボタンを表示する
+          topBtn.fadeIn();
 
-          }else{
+        }else{
 
-            //---- 画面が80pxより上なら、ボタンを表示しない
-            topBtn.fadeOut();
+          //---- 画面が80pxより上なら、ボタンを表示しない
+          topBtn.fadeOut();
 
-          }
-        });
+        }
+      });
 
 
 
-        // ◇ボタンをクリックしたら、スクロールして上に戻る
-        $(function(){
-        topBtn.click(function(){
-          $('body,html').animate({
-          scrollTop: 0},500);
-          return false;
+      // ◇ボタンをクリックしたら、スクロールして上に戻る
+      $(function(){
+      topBtn.click(function(){
+        $('body,html').animate({
+        scrollTop: 0},500);
+        return false;
 
-        });
+      });
 
-        $('.Toggle').click(function() {
-          $(this).toggleClass('active');
+      $('.Toggle').click(function() {
+        $(this).toggleClass('active');
 
-          if ($(this).hasClass('active')) {
-          $('.NavMenu').addClass('active'); //クラスを付与
-          } else {
-          $('.NavMenu').removeClass('active'); //クラスを外す
-          }
-          });
-          });
-
-        // ボタンスクロール
-  $(function(){
-        $('a[href^="#"]').click(function(){
-          var speed = 600;
-          var href= $(this).attr("href");
-          var target = $(href == "#" || href == "" ? 'html' : href);
-          var position = target.offset().top;
-          $("body,html").animate({scrollTop:position}, speed, "swing");
-          return false;
+        if ($(this).hasClass('active')) {
+        $('.NavMenu').addClass('active'); //クラスを付与
+        } else {
+        $('.NavMenu').removeClass('active'); //クラスを外す
+        }
         });
         });
-        });
+
+      // ボタンスクロール
+$(function(){
+      $('a[href^="#"]').click(function(){
+        var speed = 600;
+        var href= $(this).attr("href");
+        var target = $(href == "#how-to" || href == "id=how-to" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+      });
+      });
+      });
+      });
+
